ae8266dc95ddc2596cf5297a1c955d1934411686
David Nieto <u2903149@unimilitar.edu.co>
2023-03-25 18:51:33 -0500

    Terminacion-curso

---
+++ 12-OperadorSwitch.js
@@ -0,0 +1,41 @@
+const ciudadesDisponibles = new Array("Bogotá", "Lima", "Santiago", "Montevideo");
+const ciudadDestino = "Santiago";
+
+let valorPasaje = 0;
+
+console.log(`Verificando pasajes para ${ciudadDestino}`);
+
+
+    //condicion con if
+/*      if (ciudadDestino == "Bogotá"){
+        valorPasaje = 500;
+    } else if (ciudadDestino == "Lima"){
+        valorPasaje = 400;
+    } else if (ciudadDestino == "Santiago"){
+        valorPasaje = 380;
+    }else if (ciudadDestino == "Montevideo"){
+        valorPasaje = 200;
+    } */
+
+    switch(ciudadDestino){
+        case "Bogotá":
+            valorPasaje = 500;
+            break;
+        case "Lima":
+            valorPasaje = 400;
+            break;
+        case "Santiago":
+            valorPasaje = 380;
+            break;
+        case "Montevideo":
+            valorPasaje = 200;
+            break;
+        default:
+            console.log('No hay pasajes para la ciudad indicada');
+            break;
+    }
+    if (valorPasaje > 0){
+        console.log(`El valor del pasaje es: ${valorPasaje}`);
+    }
+
+
